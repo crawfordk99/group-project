58e8e3020b5d68d48e39dcaa91c1832cd9b44c36
Keith Crawford <121456679+crawfordk99@users.noreply.github.com>
2025-02-04 15:14:29 -0700

    Added firebase files (#3)
    * Created flutter project
    
    * Updated device file
    
    * Create .DS_Store
    
    * Added firebase files, rules, and dart packages

--- group_project/lib/main.dart
+++ group_project/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() {
   runApp(const MyApp());
