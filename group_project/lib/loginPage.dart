e15958f494fab0703e58a59b6fd27fef336bd2c1
Julio Cesar Tavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2025-03-21 16:01:58 -0600

    Removes an unnecessary function in the login page and changes the title to be verification page.
    * I am starting the authentication page
    
    * login PGE
    
    * Revert "I am starting the authentication page"
    
    This reverts commit 7e82823aac04204f2d894eb9367bf3cc293e1305.
    
    * pulling items
    
    * removing function from login page
    
    ---------
    
    Co-authored-by: Keith Crawford <121456679+crawfordk99@users.noreply.github.com>

--- group_project/lib/loginPage.dart
+++ group_project/lib/loginPage.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:group_project/services/firebase_auth_service.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login Page',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Login Page'),
-    );
-  }
-}
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  // const MyHomePage({super.key, required this.title});
+  //
+  // final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -43,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+         title: Text("Verification page"),
       ),
       body: Center(
         child: Padding(
